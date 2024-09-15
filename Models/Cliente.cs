11077f7a469bad0d8e33f37645882375149727be
Gustavo Zanotti <guzanottis@gmail.com>
2024-09-14 21:39:25 -0300

    initial commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiClienteXp.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
