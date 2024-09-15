56ebfb7c39a34cb15510a2d7c9bbaf3fcc5f159f
Gustavo Zanotti <guzanottis@gmail.com>
2024-09-15 19:05:24 -0300

    Mudanca Diretorio

---
+++ ApiClienteXpxUnitTests/UnitTests/ClientesUnitTestController.cs
@@ -0,0 +1,39 @@
+﻿using ApiClienteXp.Context;
+using ApiClienteXp.Controllers;
+using ApiClienteXp.Repositories;
+using ApiClienteXp.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClienteXpxUnitTests.UnitTests
+{
+    public class ClientesUnitTestController
+    {
+        public IClienteRepository repository;
+        public ILogger _logger;
+        public static DbContextOptions<AppDbContext> DbContextOptions { get; }
+
+        public static string connectionString = "Server=localhost;DataBase=apiclientedb;UId=root;Pwd=root";
+
+        static ClientesUnitTestController()
+        {
+            DbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public ClientesUnitTestController()
+        {
+            var context = new AppDbContext(DbContextOptions);
+            repository=new ClienteRepository(context);
+        }
+
+
+    }
+}
---
+++ ApiClienteXpxUnitTests/UnitTests/GetClienteUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using ApiClienteXp.Controllers;
+using ApiClienteXp.Repositories.Interfaces;
+using ApiClienteXp.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+namespace ApiClienteXpxUnitTests.UnitTests
+{
+    public class GetClienteUnitTests : IClassFixture<ClientesUnitTestController>
+    {
+        private readonly ClientesController _controller;
+        public GetClienteUnitTests(ClientesUnitTestController controller)
+        {
+            ILogger<ClientesController> logger = NullLogger<ClientesController>.Instance;
+            _controller = new ClientesController(controller.repository, logger);
+        }
+
+        [Fact]
+        public async Task GetClienteById_OkResult()
+        {
+            //Arrange
+            var clienteId = 1;
+
+            //Act
+            var data = await _controller.Get(clienteId);
+
+            //Assert
+            data.Result.Should().BeOfType<OkObjectResult>()
+                .Which.StatusCode.Should().Be(200);
+        }
+
+
+    }
+}
